b98ecdc7cb04a04689dc0a64a7c11a44a0d46481
alexthkim <atk3@rice.edu>
2017-08-31 15:49:11 -0500

    deleted files

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/Alex/Desktop/new-personal-site/src/layouts/index.js"
-  import data from "/Users/Alex/Desktop/new-personal-site/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
